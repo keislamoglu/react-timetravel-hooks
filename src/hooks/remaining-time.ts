2473547de04645f9f812a2246755653ebfc9411d
keislamoglu <kadiremin@gmail.com>
2020-12-25 00:43:14 +0300

    refactor(remaining-time): performance improvement

--- src/hooks/remaining-time.ts
+++ src/hooks/remaining-time.ts
@@ -1,19 +1,31 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import countdown from 'countdown'
 import { RemainingTimeHookOptions } from '../interfaces'
 
 export function useRemainingTime(endDate: Date, options: RemainingTimeHookOptions = {}): string {
   const { max = 2, units = countdown.DEFAULTS, tickDuration = 1000 } = options
-  const calculateRemaining = useCallback(() => countdown(new Date(), endDate, units, max).toString(), [
-    endDate,
-    max,
-    units,
-  ])
+  const isPast = useRef(false)
   const [remaining, setRemaining] = useState<string>()
 
+  const calculateRemaining = useCallback(() => {
+    const startDate = new Date()
+
+    if (endDate.getTime() - startDate.getTime() < 0) {
+      isPast.current = true
+    }
+
+    return isPast.current ? '' : countdown(startDate, endDate, units, max).toString()
+  }, [endDate, max, units])
+
   useEffect(() => {
+    if (isPast.current) {
+      return
+    }
+
     setRemaining(calculateRemaining())
+
     const timer = setInterval(() => setRemaining(calculateRemaining()), tickDuration)
+
     return () => clearInterval(timer)
   }, [calculateRemaining, tickDuration])
 
