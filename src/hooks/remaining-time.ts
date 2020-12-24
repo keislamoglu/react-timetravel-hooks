fc7577eb640c78d3c31cc78946522b4974a8b457
keislamoglu <kadiremin@gmail.com>
2020-12-24 23:48:49 +0300

    refactor(remaining-time): improve remaining-time hook

--- src/hooks/remaining-time.ts
+++ src/hooks/remaining-time.ts
@@ -2,16 +2,9 @@ import { useCallback, useEffect, useState } from 'react'
 import countdown from 'countdown'
 import { RemainingTimeHookOptions } from '../interfaces'
 
-type CountdownStatic = countdown.CountdownStatic
-
-export function useRemainingTime(
-  endDate: Date,
-  countdown: CountdownStatic,
-  options: RemainingTimeHookOptions = {}
-): string {
+export function useRemainingTime(endDate: Date, options: RemainingTimeHookOptions = {}): string {
   const { max = 2, units = countdown.DEFAULTS, tickDuration = 1000 } = options
   const calculateRemaining = useCallback(() => countdown(new Date(), endDate, units, max).toString(), [
-    countdown,
     endDate,
     max,
     units,
