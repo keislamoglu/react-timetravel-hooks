d9c011f928d505e3132cededf93c112ac907e34a
keislamoglu <kadiremin@gmail.com>
2021-01-05 03:50:38 +0300

    build(package): install and configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import sass from 'rollup-plugin-sass'
+import typescript from 'rollup-plugin-typescript2'
+import { uglify } from 'rollup-plugin-uglify'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [sass({ insert: true }), typescript(), uglify()],
+  external: ['react', 'countdown'],
+}
